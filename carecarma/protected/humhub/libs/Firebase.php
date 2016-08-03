11abe6075f1c722e9dc6ae3935fd581c6a18e435
New User <New User>
2016-08-03 14:58:16 -0400

    'mobile'

--- carecarma/protected/humhub/libs/Firebase.php
+++ carecarma/protected/humhub/libs/Firebase.php
@@ -43,7 +43,7 @@ class Firebase
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
 
         $result = curl_exec($ch);
-        echo $result;
+        
         curl_close($ch);
 
         return $result;
