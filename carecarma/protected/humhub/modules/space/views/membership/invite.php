4cd377837afdcc724bd4a0a8c2831794cde227df
feitracylai <feitracylai@gmail.com>
2016-10-19 17:27:15 -0400

    message notify tag

--- carecarma/protected/humhub/modules/space/views/membership/invite.php
+++ carecarma/protected/humhub/modules/space/views/membership/invite.php
@@ -15,70 +15,70 @@ use humhub\models\Setting;
 
             <br/>
 
-            <?php if (Setting::Get('internalUsersCanInvite', 'authentication_internal')) : ?>
-                <div class="text-center">
-                    <ul id="tabs" class="nav nav-tabs tabs-center" data-tabs="tabs">
-                        <li class="active tab-internal"><a href="#internal"
-                                                           data-toggle="tab"><?php echo Yii::t('SpaceModule.views_space_invite', 'Pick users'); ?></a>
-                        </li>
-                        <li class="tab-external"><a href="#external"
-                                                    data-toggle="tab"><?php echo Yii::t('SpaceModule.views_space_invite', 'Invite by email'); ?></a>
-                        </li>
-                    </ul>
-                </div>
-                <br/>
-            <?php endif; ?>
-
-            <div class="tab-content">
-                <div class="tab-pane active" id="internal">
-
-
-                    <?php echo Yii::t('SpaceModule.views_space_invite', 'To invite users to this circle, please type their names below to find and pick them.'); ?>
-
-                    <br/><br/>
-
-                    <?php echo $form->field($model, 'invite')->textInput(['id' => 'invite'])->label(false); ?>
-                    <?php
-                    // attach mention widget to it
-                    echo humhub\modules\user\widgets\UserPicker::widget(array(
-                        'inputId' => 'invite',
-                        'model' => $model, // CForm Instanz
-                        'attribute' => 'invite',
-                        'placeholderText' => Yii::t('SpaceModule.views_space_invite', 'Add an user'),
-                        'focus' => true,
-                    ));
-                    ?>
-
-                </div>
-                <?php if (Setting::Get('internalUsersCanInvite', 'authentication_internal')) : ?>
-                    <div class="tab-pane" id="external">
-                        <?php echo Yii::t('SpaceModule.views_space_invite', 'You can also invite external users, which are not registered now. Just add their e-mail addresses separated by comma.'); ?>
-                        <br/><br/>
-                        <div class="form-group">
-                            <?php echo $form->field($model, 'inviteExternal')->textArea(['rows' => '3', 'placeholder' => Yii::t('SpaceModule.views_space_invite', 'Email addresses'), 'id' => 'email_invite'])->label(false); ?>
-                        </div>
-                    </div>
-                <?php endif; ?>
-            </div>
-
+<!--            --><?php //if (Setting::Get('internalUsersCanInvite', 'authentication_internal')) : ?>
+<!--                <div class="text-center">-->
+<!--                    <ul id="tabs" class="nav nav-tabs tabs-center" data-tabs="tabs">-->
+<!--                        <li class="active tab-internal"><a href="#internal"-->
+<!--                                                           data-toggle="tab">--><?php //echo Yii::t('SpaceModule.views_space_invite', 'Pick users'); ?><!--</a>-->
+<!--                        </li>-->
+<!--                        <li class="tab-external"><a href="#external"-->
+<!--                                                    data-toggle="tab">--><?php //echo Yii::t('SpaceModule.views_space_invite', 'Invite by email'); ?><!--</a>-->
+<!--                        </li>-->
+<!--                    </ul>-->
+<!--                </div>-->
+<!--                <br/>-->
+<!--            --><?php //endif; ?>
+<!---->
+<!--            <div class="tab-content">-->
+<!--                <div class="tab-pane active" id="internal">-->
+<!---->
+<!---->
+<!--                    --><?php //echo Yii::t('SpaceModule.views_space_invite', 'To invite users to this circle, please type their names below to find and pick them.'); ?>
+<!---->
+<!--                    <br/><br/>-->
+<!---->
+<!--                    --><?php //echo $form->field($model, 'invite')->textInput(['id' => 'invite'])->label(false); ?>
+<!--                    --><?php
+//                    // attach mention widget to it
+//                    echo humhub\modules\user\widgets\UserPicker::widget(array(
+//                        'inputId' => 'invite',
+//                        'model' => $model, // CForm Instanz
+//                        'attribute' => 'invite',
+//                        'placeholderText' => Yii::t('SpaceModule.views_space_invite', 'Add an user'),
+//                        'focus' => true,
+//                    ));
+//                    ?>
+<!---->
+<!--                </div>-->
+<!--                --><?php //if (Setting::Get('internalUsersCanInvite', 'authentication_internal')) : ?>
+<!--                    <div class="tab-pane" id="external">-->
+<!--                        --><?php //echo Yii::t('SpaceModule.views_space_invite', 'You can also invite external users, which are not registered now. Just add their e-mail addresses separated by comma.'); ?>
+<!--                        <br/><br/>-->
+<!--                        <div class="form-group">-->
+<!--                            --><?php //echo $form->field($model, 'inviteExternal')->textArea(['rows' => '3', 'placeholder' => Yii::t('SpaceModule.views_space_invite', 'Email addresses'), 'id' => 'email_invite'])->label(false); ?>
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                --><?php //endif; ?>
+<!--            </div>-->
+            <p>Here will add users in your people</p>
 
         </div>
         <div class="modal-footer">
 
-            <?php
-            echo \humhub\widgets\AjaxButton::widget([
-                'label' => Yii::t('SpaceModule.views_space_invite', 'Send'),
-                'ajaxOptions' => [
-                    'type' => 'POST',
-                    'beforeSend' => new yii\web\JsExpression('function(){ setModalLoader(); }'),
-                    'success' => new yii\web\JsExpression('function(html){ $("#globalModal").html(html); }'),
-                    'url' => $space->createUrl('/space/membership/invite'),
-                ],
-                'htmlOptions' => [
-                    'class' => 'btn btn-primary'
-                ]
-            ]);
-            ?>
+<!--            --><?php
+//            echo \humhub\widgets\AjaxButton::widget([
+//                'label' => Yii::t('SpaceModule.views_space_invite', 'Send'),
+//                'ajaxOptions' => [
+//                    'type' => 'POST',
+//                    'beforeSend' => new yii\web\JsExpression('function(){ setModalLoader(); }'),
+//                    'success' => new yii\web\JsExpression('function(html){ $("#globalModal").html(html); }'),
+//                    'url' => $space->createUrl('/space/membership/invite'),
+//                ],
+//                'htmlOptions' => [
+//                    'class' => 'btn btn-primary'
+//                ]
+//            ]);
+//            ?>
             <button type="button" class="btn btn-primary"
                     data-dismiss="modal"><?php echo Yii::t('SpaceModule.views_space_invite', 'Close'); ?></button>
 
