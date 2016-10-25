a66c4040c64793e6ebd1fd0812f13d08bbc6e330
feitracylai <feitracylai@gmail.com>
2016-10-25 16:45:50 -0400

    memberlist show detail

--- carecarma/protected/humhub/modules/space/modules/manage/views/memberlist/index.php
+++ carecarma/protected/humhub/modules/space/modules/manage/views/memberlist/index.php
@@ -10,15 +10,79 @@ use yii\helpers\Html;
         <?php foreach ($members as $membership) : ?>
             <?php $user = $membership->user; ?>
             <?php if($membership->status === \humhub\modules\space\models\Membership::STATUS_MEMBER) : ?>
-                <a href="<?php echo $user->getUrl(); ?>">
+                <div href="#" id="media-<?php echo $user->guid; ?>">
                     <img src="<?php echo $user->getProfileImage()->getUrl(); ?>" class="img-rounded tt img_margin"
                          style="width: 48px; height: 48px;" data-toggle="tooltip" data-placement="top" >
-                    <?php echo Html::encode($user->displayName); ?>
-                    <hr>
-                </a>
+                    <strong><?php echo Html::encode($user->displayName); ?></strong>
+<!--                    <hr>-->
+                </div>
+
+                <div class="contactInfo" id="info-<?php echo $user->guid; ?>" hidden>
+                    <div class="middle">
+                        <?php
+                        $devicePhone = '';
+                        if ($user->device_id != null){
+                            $device = \humhub\modules\user\models\Device::findOne(['device_id' => $user->device_id]);
+                            $devicePhone = $device->phone;
+                        }
+                            echo \humhub\widgets\DetailView::widget([
+                                'model' => $user,
+                                'attributes' => [
+                                    'email:email',
+                                    [
+                                        'label' => Yii::t('SpaceModule.widgets_views_spaceMembers', 'Phone Home'),
+                                        'value' => $user->profile->phone_private,
+                                    ],
+                                    [
+                                        'label' => Yii::t('SpaceModule.widgets_views_spaceMembers', 'Phone Work'),
+                                        'value' => $user->profile->phone_work,
+                                    ],
+                                    [
+                                        'label' => Yii::t('SpaceModule.widgets_views_spaceMembers', 'Cosmos Phone'),
+                                        'value' => $devicePhone,
+                                    ],
+                                    [
+                                        'label' => Yii::t('SpaceModule.widgets_views_spaceMembers', 'Street'),
+                                        'value' => $user->profile->street,
+                                    ],
+                                    [
+                                        'label' => Yii::t('SpaceModule.widgets_views_spaceMembers', 'Apt/Unit'),
+                                        'value' => $user->profile->street2,
+//                                        'value' => $user->profile->unit,
+                                    ],
+                                    [
+                                        'label' => Yii::t('SpaceModule.widgets_views_spaceMembers', 'City'),
+                                        'value' => $user->profile->city,
+                                    ],
+                                    [
+                                        'label' => Yii::t('SpaceModule.widgets_views_spaceMembers', 'State'),
+                                        'value' => $user->profile->state,
+                                    ],
+                                    [
+                                        'label' => Yii::t('SpaceModule.widgets_views_spaceMembers', 'Country'),
+                                        'value' => $user->profile->country,
+                                    ],
+                                ],
+                                'options' => [
+                                    'class' => 'table member-view',
+                                    'style' => 'margin: 10px 0; border-top: none',
+                                ]
+                            ]);
+                        ?>
+                    </div>
+                </div>
+                <script type="text/javascript">
+                    $('#media-<?php echo $user->guid; ?>').click(function(){
+
+                        $('#info-<?php echo $user->guid; ?>').toggle();
+
+                    })
+                </script>
+                <hr>
             <?php endif; ?>
         <?php endforeach; ?>
 
 
     </div>
 </div>
+
