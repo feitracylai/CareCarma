034af7cca7b92c6c4c65c9f293f75d354344d19a
Xinlong Xu <Simon@Xinlongs-MacBook-Pro.local>
2016-10-28 13:11:10 -0700

    update

--- carecarma/protected/humhub/modules/user/views/contact/add.php
+++ carecarma/protected/humhub/modules/user/views/contact/add.php
@@ -24,7 +24,7 @@ use \humhub\modules\space\models\Space;
             <?php echo Html::a('<i class="fa fa-send"></i> '.Yii::t('UserModule.views_contact_add', 'Import from Device'), $thisUser->createUrl('/user/contact/importlocal'), array('class' => 'btn btn-primary')); ?>
 
 <!--        <div class="pull-right">-->
-            <?php echo Html::a('<i class="fa fa-send"></i> '.Yii::t('UserModule.views_contact_add', 'Import from Google'), "https://accounts.google.com/o/oauth2/auth?client_id=584594431619-c8gb5m52css0vs8biotp7jcie27h0iff.apps.googleusercontent.com&redirect_uri=http://127.0.0.1:8888/CareCarma/carecarma/index.php?r=user%2Fcontact%2Fimportgoogle&scope=https://www.google.com/m8/feeds/&response_type=code", array('class' => 'btn btn-primary')); ?>
+            <?php echo Html::a('<i class="fa fa-send"></i> '.Yii::t('UserModule.views_contact_add', 'Import from Google'), "https://accounts.google.com/o/oauth2/auth?client_id=584594431619-c8gb5m52css0vs8biotp7jcie27h0iff.apps.googleusercontent.com&redirect_uri=http://www.carecarma.tk/carecarma/index.php?r=user%2Fcontact%2Fimportgoogle&scope=https://www.google.com/m8/feeds/&response_type=code", array('class' => 'btn btn-primary')); ?>
         </div>
         </br>
 
