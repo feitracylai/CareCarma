a3e2fb5a83129ef4686e42afba7b64bb3bbee82e
feitracylai <feitracylai@gmail.com>
2016-11-02 14:17:46 -0400

    debug

--- carecarma/protected/humhub/modules/calendar/Events.php
+++ carecarma/protected/humhub/modules/calendar/Events.php
@@ -37,10 +37,10 @@ class Events extends \yii\base\Object
         $space = $event->sender->space;
         if ($space->isModuleEnabled('calendar')) {
             $event->sender->addItem(array(
-                'label' => Yii::t('CalendarModule.base', 'Calendar'),
+                'label' => Yii::t('CalendarModule.base', 'Circle Calendar'),
                 'group' => 'modules',
                 'url' => $space->createUrl('/calendar/view/index'),
-                'icon' => '<i class="fa fa-calendar"></i>',
+                'icon' => '<i class="fa fa-calendar-check-o"></i>',
                 'isActive' => (Yii::$app->controller->module && Yii::$app->controller->module->id == 'calendar'),
             ));
         }
