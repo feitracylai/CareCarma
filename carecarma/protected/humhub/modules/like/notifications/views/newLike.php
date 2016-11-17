444aecc6c5de4857d1eab9469f7753fc3390ea70
root <root@ip-10-170-173-73.ec2.internal>
2016-11-17 19:15:53 +0000

    test

--- carecarma/protected/humhub/modules/like/notifications/views/newLike.php
+++ carecarma/protected/humhub/modules/like/notifications/views/newLike.php
@@ -1,10 +1,8 @@
 <?php
 
 use yii\helpers\Html;
-use yii\log\Logger;
 
 
-    \Yii::getLogger()->log($originator->displayName, Logger::LEVEL_INFO, 'MyLog');
     echo Yii::t('LikeModule.views_notifications_newLike', "%displayName% likes %contentTitle%.", array(
         '%displayName%' => '<strong>' . Html::encode($originator->displayName) . '</strong>',
         '%contentTitle%' => $this->context->getContentInfo($source->getSource())
