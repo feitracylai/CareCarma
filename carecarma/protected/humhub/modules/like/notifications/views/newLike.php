0a56ef0174298f4ffda98553b0c24e4fc11ef178
feitracylai <feitracylai@gmail.com>
2016-11-17 13:38:04 -0500

    like test

--- carecarma/protected/humhub/modules/like/notifications/views/newLike.php
+++ carecarma/protected/humhub/modules/like/notifications/views/newLike.php
@@ -3,15 +3,13 @@
 use yii\helpers\Html;
 use yii\log\Logger;
 
-if (!isset($source->getSource) || $source->getSource() == null){
-    \Yii::getLogger()->log($originator->displayName, Logger::LEVEL_INFO, 'MyLog');
-} else{
+
     \Yii::getLogger()->log($originator->displayName, Logger::LEVEL_INFO, 'MyLog');
     echo Yii::t('LikeModule.views_notifications_newLike', "%displayName% likes %contentTitle%.", array(
         '%displayName%' => '<strong>' . Html::encode($originator->displayName) . '</strong>',
         '%contentTitle%' => $this->context->getContentInfo($source->getSource())
     ));
-}
+
 
 
 ?>
