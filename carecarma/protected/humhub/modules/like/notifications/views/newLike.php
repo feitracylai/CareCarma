4e26337c8f87f3811a3296573b38a4e88f90bc38
feitracylai <feitracylai@gmail.com>
2016-11-17 13:28:08 -0500

    test

--- carecarma/protected/humhub/modules/like/notifications/views/newLike.php
+++ carecarma/protected/humhub/modules/like/notifications/views/newLike.php
@@ -3,9 +3,10 @@
 use yii\helpers\Html;
 use yii\log\Logger;
 
-if ($source->getSource() == null){
+if (!isset($source->getSource) || $source->getSource() == null){
     \Yii::getLogger()->log($originator->displayName, Logger::LEVEL_INFO, 'MyLog');
 } else{
+    \Yii::getLogger()->log($originator->displayName, Logger::LEVEL_INFO, 'MyLog');
     echo Yii::t('LikeModule.views_notifications_newLike', "%displayName% likes %contentTitle%.", array(
         '%displayName%' => '<strong>' . Html::encode($originator->displayName) . '</strong>',
         '%contentTitle%' => $this->context->getContentInfo($source->getSource())
