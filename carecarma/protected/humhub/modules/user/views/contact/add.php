4639753735f8dc26cec6b0e571c1e1bc561cff4e
feitracylai <feitracylai@gmail.com>
2016-11-30 18:07:40 -0500

    test

--- carecarma/protected/humhub/modules/user/views/contact/add.php
+++ carecarma/protected/humhub/modules/user/views/contact/add.php
@@ -12,7 +12,46 @@ use \humhub\modules\space\models\Space;
 
 Url::remember();
 
-
+if (strpos($_SERVER['HTTP_USER_AGENT'], 'Maxthon')) {
+    $browser = 'Maxthon';
+} elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 12.0')) {
+    $browser = 'IE12.0';
+} elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 11.0')) {
+    $browser = 'IE11.0';
+} elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 10.0')) {
+    $browser = 'IE10.0';
+} elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 9.0')) {
+    $browser = 'IE9.0';
+} elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 8.0')) {
+    $browser = 'IE8.0';
+} elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 7.0')) {
+    $browser = 'IE7.0';
+} elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 6.0')) {
+    $browser = 'IE6.0';
+} elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'NetCaptor')) {
+    $browser = 'NetCaptor';
+} elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Netscape')) {
+    $browser = 'Netscape';
+} elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Lynx')) {
+    $browser = 'Lynx';
+} elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Opera')) {
+    $browser = 'Opera';
+} elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome')) {
+    $browser = 'Google';
+} elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Firefox')) {
+    $browser = 'Firefox';
+} elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Safari')) {
+    $browser = 'Safari';
+} elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'iphone') || strpos($_SERVER['HTTP_USER_AGENT'], 'ipod')) {
+    $browser = 'iphone';
+} elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'ipad')) {
+    $browser = 'iphone';
+} elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'android')) {
+    $browser = 'android';
+} else {
+    $browser = 'other';
+}
+Yii::getLogger()->log($browser, \yii\log\Logger::LEVEL_INFO, 'MyLog');
 
 ?>
 
