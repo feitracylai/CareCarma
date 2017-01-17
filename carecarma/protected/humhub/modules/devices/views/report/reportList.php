7d9dfbe7fbdb5e5c842392b976c6aa99650885a1
feitracylai <feitracylai@gmail.com>
2017-01-17 09:58:16 -0500

    report notify show debug

--- carecarma/protected/humhub/modules/devices/views/report/reportList.php
+++ carecarma/protected/humhub/modules/devices/views/report/reportList.php
@@ -4,7 +4,7 @@
 use humhub\modules\space\models\Membership;
 use humhub\modules\space\models\Space;
 
-
+$hasCR = false;
 ?>
 
 <?php if (count($space_list) != 0) : ?>
@@ -15,6 +15,7 @@ use humhub\modules\space\models\Space;
         if (count($space_members) != 0){
             foreach ($space_members as $space_member){
 //                if ($space_member->user_id == )
+                $hasCR = true;
                 $isNew = false;
                 echo $this->render('_receiverPreview', array('userId' => $space_member->user_id, 'spaceId' => $space_member->space_id, 'isNew' => $isNew));
             }
@@ -22,6 +23,10 @@ use humhub\modules\space\models\Space;
 
         ?>
     <?php endforeach; ?>
-<?php else: ?>
+
+<?php endif; ?>
+
+
+<?php if (!$hasCR): ?>
     <li class="placeholder"> <?php echo Yii::t('DevicesModule.views_report_list', 'There are no receiver in your circles now.'); ?></li>
 <?php endif; ?>
