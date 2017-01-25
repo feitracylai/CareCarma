1ea22f71f3c2435e02dfa99c7faa78745f465664
Xinlong Xu <simonxu14@gmail.com>
2017-01-25 15:03:00 -0800

    test

--- carecarma/protected/humhub/modules/user/controllers/SensorController.php
+++ carecarma/protected/humhub/modules/user/controllers/SensorController.php
@@ -1449,6 +1449,7 @@ class SensorController extends Controller
     public function actionTestbytes()
     {
         Yii::getLogger()->log(print_r("data",true),yii\log\Logger::LEVEL_INFO,'MyLog');
+        Yii::getLogger()->log(print_r($_POST,true),yii\log\Logger::LEVEL_INFO,'MyLog');
         $data = Yii::$app->request->post();
         Yii::getLogger()->log(print_r($data,true),yii\log\Logger::LEVEL_INFO,'MyLog');
         $pure_data = $data['Sensor'];
