cdd8c6e7494a0baa9602b795cb42566333a0a3a6
feitracylai <feitracylai@gmail.com>
2017-02-23 10:35:23 -0500

    custom_page

---
+++ carecarma/protected/humhub/modules/custom_pages/messages/cs/views_admin_edit.php
@@ -0,0 +1,12 @@
+<?php
+return array (
+  '<strong>Create</strong> page' => '<strong>Vytvořit</strong> stránku',
+  '<strong>Edit</strong> page' => '<strong>Změnit</strong> stránku',
+  'Content' => 'Obsah',
+  'Default sort orders scheme: 100, 200, 300, ...' => 'Výchozí schéma řazení: 100, 200, 300...',
+  'Delete' => 'Smazat',
+  'Page title' => 'Titulek stránky',
+  'Save' => 'Uložit',
+  'Sort Order' => 'Řazení',
+  'URL' => 'Adresa odkazu',
+);
---
+++ carecarma/protected/humhub/modules/custom_pages/messages/fr/base.php
@@ -0,0 +1,17 @@
+<?php
+return array (
+  'Create new Page' => 'Créer une nouvelle page',
+  'Custom Pages' => 'Pages personnalisées',
+  'HTML' => 'HTML',
+  'IFrame' => 'IFrame',
+  'Link' => 'Lien',
+  'MarkDown' => 'MarkDown',
+  'Navigation' => 'Menu de navigation',
+  'No custom pages created yet!' => 'Aucune page personnalisée n\'a été créée !',
+  'Sort Order' => 'Ordre de tri',
+  'Title' => 'Titre',
+  'Top Navigation' => 'Menu de navigation',
+  'Type' => 'Type',
+  'User Account Menu (Settings)' => 'Menu du compte utilisateur (paramètres)',
+  'Without adding to navigation (Direct link)' => 'Sans l\'ajouter au menu de navigation (lien direct)',
+);
