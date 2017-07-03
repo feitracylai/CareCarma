e99ce4e3b4a0a393f6c356e3f1c537ed3cf9dd5c
feitracylai <feitracylai@gmail.com>
2017-07-03 10:50:28 -0400

    test & debug

--- carecarma/protected/humhub/modules/dashboard/views/dashboard/index.php
+++ carecarma/protected/humhub/modules/dashboard/views/dashboard/index.php
@@ -3,7 +3,11 @@ $this->pageTitle = Yii::t('DashboardModule.views_dashboard_index', 'Home');
 ?>
 <div class="container">
     <div class="row">
+        <?php if (Yii::$app->user->id == 1): ?>
+        <div class="col-md-8 layout-below-top-second">
+        <?php else: ?>
         <div class="col-md-12 layout-below-top-second">
+        <?php endif; ?>
             <?php
             if ($showProfilePostForm) {
                 echo \humhub\modules\post\widgets\Form::widget(['contentContainer' => \Yii::$app->user->getIdentity()]);
@@ -18,12 +22,14 @@ $this->pageTitle = Yii::t('DashboardModule.views_dashboard_index', 'Home');
             ]);
             ?>
         </div>
-        <!--<div class="col-md-4 layout-sidebar-container">
+        <?php if (Yii::$app->user->id == 1): ?>
+        <div class="col-md-4 layout-sidebar-container">
             <?php
-            //echo \humhub\modules\dashboard\widgets\Sidebar::widget(['widgets' => [
-            //        [\humhub\modules\activity\widgets\Stream::className(), ['streamAction' => '/dashboard/dashboard/stream'], ['sortOrder' => 150]]
-            //]]);
+            echo \humhub\modules\dashboard\widgets\Sidebar::widget(['widgets' => [
+                    [\humhub\modules\activity\widgets\Stream::className(), ['streamAction' => '/dashboard/dashboard/stream'], ['sortOrder' => 150]]
+            ]]);
             ?>
-        </div>-->
+        </div>
+        <?php endif; ?>
     </div>
 </div>
